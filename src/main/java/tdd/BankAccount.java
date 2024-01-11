ecfd78566a64490b2ba6e3d0327c8c0bc17e0b50
Gibier Francois <francois.gibier.etu@univ-lille.fr>
2024-01-11 21:18:34 +0100

    3.5 Implémentation

--- src/main/java/tdd/BankAccount.java
+++ src/main/java/tdd/BankAccount.java
@@ -4,26 +4,65 @@ import lombok.Getter;
 
 public class BankAccount {
     @Getter
-    private double credit = 0;
+    private double[] credits;
 
     @Getter
-    private double debit = 0;
+    private double[] debits;
+
+    public BankAccount(int historiesSize) {
+        this.credits = new double[historiesSize];
+        this.debits = new double[historiesSize];
+    }
 
     public void credit(double creditValue) throws InvalidValueException {
-        this.credit = addIfSuperiorToZero(this.credit, creditValue);
+        this.credits = addValueInArray(this.credits, creditValue);
     }
 
     public void debit(double debitValue) throws InvalidValueException {
-        this.debit = addIfSuperiorToZero(this.debit, debitValue);
+        this.debits = addValueInArray(this.debits, debitValue);
     }
 
-    private static double addIfSuperiorToZero(double baseValue, double addedValue) throws InvalidValueException {
+    private static double[] addValueInArray(double[] array, double addedValue) throws InvalidValueException {
         if (addedValue < 0)
             throw new InvalidValueException();
-        return baseValue + addedValue;
+        int index = findFirstZeroInArray(array);
+        if (index != -1)
+            array[index] = addedValue;
+        else {
+            double[] newArray = new double[array.length];
+            newArray[0] = sum(array) + addedValue;
+            array = newArray;
+        }
+        return array;
+    }
+
+    private static int findFirstZeroInArray(double[] array) {
+        int cpt = 0;
+        for (double d : array) {
+            if (d == 0)
+                return cpt;
+            cpt++;
+        }
+        return -1;
     }
 
     public double computeBalance() {
-        return this.credit - this.debit;
+        return sum(this.credits) - sum(this.debits);
+    }
+
+    private static double sum(double[] array) {
+        double sum = 0;
+        for (double d : array) {
+            sum += d;
+        }
+        return sum;
+    }
+
+    public double getCredit(int i) {
+        return this.credits[i];
+    }
+
+    public double getDebit(int i) {
+        return this.debits[i];
     }
 }
