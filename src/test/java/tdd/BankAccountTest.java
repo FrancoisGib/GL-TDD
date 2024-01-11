8e6875f838c40a5feb40a449a3c631d46eb9e4b4
Gibier Francois <francois.gibier.etu@univ-lille.fr>
2024-01-11 20:43:22 +0100

    3.3 Test

--- src/test/java/tdd/BankAccountTest.java
+++ src/test/java/tdd/BankAccountTest.java
@@ -1,6 +1,7 @@
 package tdd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,16 +21,29 @@ public class BankAccountTest {
     }
 
     @Test
-    public void verifyCreditIncrementationWhenCreditingAccount() {
+    public void verifyCreditIncrementationWhenCreditingAccount() throws InvalidValueException {
         double creditValue = 1;
         account.credit(creditValue);
         assertEquals(1, account.getCredit());
     }
 
     @Test
-    public void verifyDebitIncrementationWhenDebitingAccount() {
+    public void verifyDebitIncrementationWhenDebitingAccount() throws InvalidValueException {
         double debitValue = 1;
         account.debit(debitValue);
         assertEquals(1, account.getDebit());
     }
+
+    @Test
+    public void verifyInvalidValueExceptionThrowsWhenCreditingNegativeValue() {
+        double creditValue = -1;
+        assertThrows(InvalidValueException.class, () -> account.credit(creditValue));
+    }
+
+    @Test
+    public void verifyInvalidValueExceptionThrowsWhenDebitingNegativeValue() {
+        double debitValue = -1;
+        assertThrows(InvalidValueException.class, () -> account.debit(debitValue));
+    }
 }
+
