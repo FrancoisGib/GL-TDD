88185ee40cdc66401115db195c2afd81fa55eeef
Gibier Francois <francois.gibier.etu@univ-lille.fr>
2024-01-12 23:34:13 +0100

    4.3 Implémentation

--- src/main/java/tdd/SavingAccount.java
+++ src/main/java/tdd/SavingAccount.java
@@ -1,10 +1,21 @@
 package tdd;
 
 public class SavingAccount extends BankAccount {
+    private double interestsRatio;
+
+    public SavingAccount(double interestsRatio) {
+        this.interestsRatio = interestsRatio;
+    }
+
     @Override
     public void debit(double debitValue) throws Exception {
         if (this.computeBalance() < debitValue)
             throw new DebitSuperiorToBalanceException();
         super.debit(debitValue);
     }
+
+    private double computeInterests() {
+        return this.computeBalance() * this.interestsRatio;
+    }
 }
+
